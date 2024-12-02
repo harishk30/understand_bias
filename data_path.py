026972b03d2d395854b99ebe960f360790cd1af3
Boya Zeng <boyazeng@sas.upenn.edu>
2024-12-02 03:11:13 -0500

    Initial commit

---
+++ data_path.py
@@ -0,0 +1,19 @@
+IMAGE_ROOTS = {
+    'cc': '',
+    'datacomp': '',
+    'yfcc': '',
+}
+
+SAVE_ROOTS = {
+    'canny': '',
+    'caption': '',
+    'depth': '',
+    'hog': '',
+    'object_det': '',
+    'sam': '',
+    'seg': '',
+    'sift': '',
+    'text_to_image': '',
+    'uncon_gen': '',
+    'vae': '',
+}
